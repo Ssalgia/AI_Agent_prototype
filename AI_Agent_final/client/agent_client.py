dda2aa3a8a230a3723e102b63ad93607022a0fc1
Ssalgia <37534160+Ssalgia@users.noreply.github.com>
2025-04-01 14:39:10 +0530

    Update agent_client.py

--- AI_Agent_final/client/agent_client.py
+++ AI_Agent_final/client/agent_client.py
@@ -1,12 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import grpc
 import agent_pb2
 import agent_pb2_grpc
-
-
 
 def send_data(agent_name, data):
     with grpc.insecure_channel('localhost:50051') as channel:
